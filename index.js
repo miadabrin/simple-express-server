9674c63fdc54797e19e97fdf2cc2b44cf89caa1a
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-07-30 15:10:32 +0300

    10 of 11

--- index.js
+++ index.js
@@ -2,17 +2,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var validate = require('express-jsonschema').validate;
 app.use(bodyParser.json())
-
-// Listen on this port
-var PORT = 3000;
-var VERSION = 1;
-
-// This will have all the todos, no need to save it between restarts.
-var todos = [];
-
-// TODO Write server here
-
 var TodoSchema = {
     type: 'object',
     properties: {
@@ -27,6 +18,16 @@ var TodoSchema = {
     }
 }
 
+
+// Listen on this port
+var PORT = 3000;
+var VERSION = 1;
+
+// This will have all the todos, no need to save it between restarts.
+var todos = [];
+
+// TODO Write server here
+
 //Version
 app.get('/', function (req, res) {
   res.send('Version:' + VERSION);
@@ -36,7 +37,7 @@ app.get('/todos', function (req, res) {
   res.send(todos);
 });
 
-app.post('/todos', function (req, res) {
+app.post('/todos', validate({body: TodoSchema}), function (req, res) {
 	todos.push(req.body);
 	let location = req.url + '/' + (todos.length - 1)
 	res.status(201).send(location);
@@ -52,6 +53,18 @@ app.delete('/todos/:index',function (req, res) {
 	res.status(204).send();
 });
 
+
+app.use(function(err, req, res, next) {
+    var responseData;
+    if (err.name === 'JsonSchemaValidation') {
+        res.status(400).send('');
+    } else {
+        // pass error to next error middleware handler 
+        next(err);
+    }
+});
+
 app.listen(PORT, function () {
   console.log('Backend challange app listening on port 3000!');
 });
+
