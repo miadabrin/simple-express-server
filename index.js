78a0b1da1c7e14cae9e5a8c062f38a7723f91f3f
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-07-30 10:01:31 +0300

    server running

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
+var express = require('express');
+var app = express();
 
 // Listen on this port
 var PORT = 3000;
+var VERSION = 1;
 
 // This will have all the todos, no need to save it between restarts.
 var todos = [];
 
 // TODO Write server here
+
+
+//Version
+app.get('/', function (req, res) {
+  res.send('Version:' + VERSION);
+});
+
+app.listen(PORT, function () {
+  console.log('Backend challange app listening on port 3000!');
+});
