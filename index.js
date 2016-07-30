7e8e6c82d2101ee28199d9e13365a6bfeba9769c
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-07-30 12:53:32 +0300

    5 out of 6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json())
 
 // Listen on this port
 var PORT = 3000;
@@ -15,6 +17,15 @@ var todos = [];
 //Version
 app.get('/', function (req, res) {
   res.send('Version:' + VERSION);
+});
+
+app.get('/todos', function (req, res) {
+  res.send(todos);
+});
+
+app.post('/todos',function (req, res) {
+	todos.push(req.body);
+	res.status(201).send({});
 });
 
 app.listen(PORT, function () {
